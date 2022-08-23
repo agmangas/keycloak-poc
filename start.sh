0b5e69455e107b34a7d055b92b77cb8b766e0860
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2022-08-23 11:47:30 +0200

    Stop using the Helm chart

--- start.sh
+++ start.sh
@@ -9,14 +9,10 @@ docker-compose up -d
 
 sleep 10
 
-kubectl apply -f ./pgsecret.yml
-kubectl get secret pgsecret -o yaml
-
-PG_HOST=$(ifconfig |
+export KC_DB_URL_HOST=$(ifconfig |
     grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' |
     grep -Eo '([0-9]*\.){3}[0-9]*' |
     grep -v '127.0.0.1' |
     head -n 1)
 
-helm install keycloak-poc -f keycloak.yml bitnami/keycloak \
-    --set externalDatabase.host=${PG_HOST}
+kubectl apply -k ./
